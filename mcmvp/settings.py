8e4f926602cbf4f47fd33059a766f89a05b7db60
Daniel Lo <kennykunx@gmail.com>
2021-04-06 10:27:42 +0100

    settings changes

--- mcmvp/settings.py
+++ mcmvp/settings.py
@@ -24,12 +24,12 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'env("SECRET_KEY")'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
